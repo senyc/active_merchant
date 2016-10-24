d31a727fa0118501ad0bc6eeb30045fee42eed05
Drew Bomhof <syncrou@users.noreply.github.com>
2016-10-24 06:22:24 -0400

    Update response.rb
    Forced the addition of *** TEST TRANSACTION ***
    on message for all test transactions
    
    This is because Transnational does not return the test 
    notification.

--- lib/active_merchant/billing/response.rb
+++ lib/active_merchant/billing/response.rb
@@ -13,6 +13,10 @@ module ActiveMerchant #:nodoc:
       def test?
         @test
       end
+      
+      def message
+        test? ? "#{message} *** TEST TRANSACTION ***" : message
+      end
 
       def fraud_review?
         @fraud_review
