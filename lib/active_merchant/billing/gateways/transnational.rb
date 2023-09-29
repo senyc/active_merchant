ce45913e7ca1b8277c508ffd6fb8ae6b0325b7d0
senyc <95313103+senyc@users.noreply.github.com>
2023-09-29 09:32:19 -0400

    feat: add playground url

--- lib/active_merchant/billing/gateways/transnational.rb
+++ lib/active_merchant/billing/gateways/transnational.rb
@@ -1,10 +1,9 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class TransnationalGateway < NetworkMerchantsGateway
-      self.homepage_url = 'http://www.tnbci.com/'
+      self.homepage_url = 'sandbox.gotnpgateway.com'
       self.display_name = 'Transnational'
       self.supported_countries = ["US"]
     end
   end
 end
-
