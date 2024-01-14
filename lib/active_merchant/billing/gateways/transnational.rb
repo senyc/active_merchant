87fe29e1213fcd7f19f8853a3ab0d2e637803d35
senyc <95313103+senyc@users.noreply.github.com>
2024-01-13 20:40:44 -0500

    fix: correct method spelling

--- lib/active_merchant/billing/gateways/transnational.rb
+++ lib/active_merchant/billing/gateways/transnational.rb
@@ -6,16 +6,9 @@ module ActiveMerchant # :nodoc:
       self.supported_countries = ['US']
 
       def initialize(options = {})
-        requies!(options, :secret_key)
+        requires!(options, :secret_key)
         super
       end
-
-      # def purchase(amount, payment_method, options = {})
-      #   MultiResponse.run do |r|
-      #     r.process { authorize(amount, payment_method, options) }
-      #     r.process { capture(amount, r.authorization, options) }
-      #   end
-      # end
 
       def headers
         {
