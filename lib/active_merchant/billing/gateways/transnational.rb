8982f1fa4dad37529d0fbe3f3478af46a0f5ecd5
senyc <95313103+senyc@users.noreply.github.com>
2024-01-13 20:07:03 -0500

    feat: add correct uri

--- lib/active_merchant/billing/gateways/transnational.rb
+++ lib/active_merchant/billing/gateways/transnational.rb
@@ -1,7 +1,7 @@
 module ActiveMerchant # :nodoc:
   module Billing # :nodoc:
     class TransnationalGateway < Gateway
-      self.homepage_url = 'sandbox.gotnpgateway.com/'
+      self.homepage_url = 'https://sandbox.fluidpay.com/api/transaction'
       self.display_name = 'Transnational'
       self.supported_countries = ['US']
 
